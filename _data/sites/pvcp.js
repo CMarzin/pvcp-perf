8bce783f424b355f194aaa2b218e67093849c11c
corentinmarzin <corentin.marzin@colorz.fr>
2021-10-11 10:37:19 +0200

    [ADD] first commit

---
+++ _data/sites/pvcp.js
@@ -0,0 +1,25 @@
+module.exports = {
+	name: "PVCP", // optional, falls back to object key
+	description: "Liste des sites PVCP",
+	options: {
+		frequency: 60 * 23, // 23 hours
+		// Use "run" if the sites don’t share assets on the same origin
+		//           and we can reset chrome with each run instead of
+		//           each site in every run (it’s faster)
+		// Use "site" if sites are all on the same origin and share assets.
+		freshChrome: "site"
+	},
+	urls: [
+		"https://www.centerparcs.fr/",
+		"https://www.centerparcs.be/be-wl",
+		"https://www.centerparcs.be/be-vl",
+		"https://www.centerparcs.be/ch-cf",
+		"https://www.centerparcs.be/ch-cd",
+		"https://www.centerparcs.de/",
+		"https://www.centerparcs.nl/",
+		"https://www.centerparcs.eu/",
+		"https://www.centerparcs.co.uk/",
+		"https://www.centerparcs.pl",
+		"https://www.pierreetvacances.com/"
+	]
+};
